35a3088a8fb54f55a622630677f7ed119f3cabc4
Erik M Baxstrom <baxstrem@gmail.com>
2023-01-17 12:58:51 -0800

    blog cards displaying

--- src/components/layout/Main/Main.js
+++ src/components/layout/Main/Main.js
@@ -1,7 +1,14 @@
 import { useBlogs } from '../../../hooks/useBlogs';
+import BlogCard from '../../../components/BlogCard/BlogCard';
 
 export default function Main() {
   const blogs = useBlogs();
 
-  return <main>{blogs.map((blog) => console.log('blog', blog))}</main>;
+  return (
+    <main>
+      {blogs.map((blog) => (
+        <BlogCard key={blog.id} {...blog} />
+      ))}
+    </main>
+  );
 }
